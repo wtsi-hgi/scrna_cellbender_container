5acfe5e5f11e06f1345c14e78c2997d7641956e8
Guillaume G Noell <gn5@sanger.ac.uk>
2021-05-20 14:20:16 +0100

    auto-git

--- get_from_dockerhub.sh
+++ get_from_dockerhub.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/env bash                                                                                                                         
-                                                                                                                                            
+#!/usr/bin/env bash
 # this script is placed/run at /lustre/scratch123/hgi/projects/ukbb_scrna/pipelines/singularity_images/ 
 
 # choose dockerhub image tag
-# cf. https://github.com/wtsi-hgi/scrna_deconvolution_container
-tag=1.0
+# cf. https://github.com/wtsi-hgi/nf_cellbender_container
+tag=v1.1
 
 mkdir -p cache_dir
 export SINGULARITY_CACHEDIR=$PWD/cache_dir
@@ -12,9 +11,8 @@ export SINGULARITY_CACHEDIR=$PWD/cache_dir
 mkdir -p tmp_dir
 export TMPDIR=$PWD/tmp_dir
 
-rm -f scrna_deconvolution_latest.sif || true
-singularity pull docker://mercury/scrna_deconvolution:${tag}
+rm -f nf_cellbender${tag}.sif || true
+rm -f nf_cellbender${tag}.img || true
 
-# place as 'latest' tag for FCE scrna deconvolution pipeline to pick up.
-# see https://gitlab.internal.sanger.ac.uk/gn5/single-cell-nextflow-ci/-/blob/deconv_nogenotype_sbw_Deconvolution_Exp4/wr_cloud_script.sh
-ln -s scrna_deconvolution_${tag}.sif scrna_deconvolution_latest.sif
+singularity pull docker://wtsihgi/nf_cellbender:${tag}
+mv nf_cellbender${tag}.sif nf_cellbender${tag}.img
