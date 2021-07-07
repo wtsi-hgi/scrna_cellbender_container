c2f5129a34fd0aaa0c9c8a916fdf6c89430f2b06
gn5 <gn5@sanger.ac.uk>
2021-07-07 16:38:45 +0000

    auto-git

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,5 @@
-nohup sudo docker build . -t wtsihgi/nf_cellbender:v1.2 &
+rm nohup.out && nohup sudo docker build . -t wtsihgi/nf_cellbender:v1.3 & sleep 2 && tail -f nohup.out
+
 docker login
-docker push wtsihgi/nf_cellbender:v1.2
+
+docker push wtsihgi/nf_cellbender:v1.3
